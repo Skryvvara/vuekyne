9f147eb74a90f99e03e1bdf93322baa6adb15ac6
Dimitri Kaiser <darkyne.gaming@outlook.de>
2021-10-14 20:16:34 +0200

    refactor(nuxt): replaced vue spa with basic nuxt template

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,24 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  verbose: true,
-};
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'vue',
+    'json'
+  ],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ],
+  testEnvironment: 'jsdom'
+}
